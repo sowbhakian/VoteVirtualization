8dbcdb16019ccd7acdeee0b56d08c6e08ad2db27
Sowbhakian_shan <74697620+sowbhakian@users.noreply.github.com>
2021-08-16 22:43:55 +0530

    Completed file uploaded.

---
+++ app.js
@@ -0,0 +1,74 @@
+// Home-Toggle-Steps
+function fstep1() {
+    //  Hide 
+
+    $("#index-flip-steps").fadeOut();
+    $("#index-flip-steps").fadeIn();
+
+    $("#step1").addClass("active")
+    $("#step2").removeClass("active")
+    $("#step3").removeClass("active")
+
+    $("#step-p1").removeClass("deactive")
+    $("#step-p2").addClass("deactive")
+    $("#step-p3").addClass("deactive")
+}
+
+function fstep2() {
+
+    $("#index-flip-steps").fadeOut();
+    $("#index-flip-steps").fadeIn();
+
+    $("#step1").removeClass("active")
+    $("#step2").addClass("active")
+    $("#step3").removeClass("active")
+
+    $("#step-p1").addClass("deactive")
+    $("#step-p2").removeClass("deactive")
+    $("#step-p3").addClass("deactive")
+}
+
+function fstep3() {
+
+    $("#index-flip-steps").fadeOut();
+    $("#index-flip-steps").fadeIn();
+
+    $("#step1").removeClass("active")
+    $("#step2").removeClass("active")
+    $("#step3").addClass("active")
+
+    $("#step-p1").addClass("deactive")
+    $("#step-p2").addClass("deactive")
+    $("#step-p3").removeClass("deactive")
+}
+
+// Enroll-Toggle
+function enrollshow(user) {
+    if (user === 'nominee') {
+        $("#nominee").removeClass("deactive")
+        $("#voters").addClass("deactive")
+    } else if (user === 'voters') {
+        $("#nominee").addClass("deactive")
+        $("#voters").removeClass("deactive")
+
+    } else {
+        console.log("Enroll Toggle Error " + user);
+
+    }
+    $("#enr-start").addClass("deactive")
+}
+
+// nominee-voters
+function next1() {
+    $("#part1").addClass("deactive");
+    $("#part2").removeClass("deactive");
+    $("#btn2-head").addClass("addblue")
+    $("#enr-part-head").text("Party Details")
+}
+
+function pre1() {
+    $("#part1").removeClass("deactive");
+    $("#part2").addClass("deactive");
+    $("#btn2-head").removeClass("addblue")
+    $("#enr-part-head").text("Personal Details")
+}
